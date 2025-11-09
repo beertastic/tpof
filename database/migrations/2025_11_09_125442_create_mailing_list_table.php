68a2efdceb204e28b53e797dc8e548c31ec62055
Tris <beertastic@gmail.com>
2025-11-09 13:18:12 +0000

    adding model for mailing list

--- database/migrations/2025_11_09_125442_create_mailing_list_table.php
+++ database/migrations/2025_11_09_125442_create_mailing_list_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('mailing_list', function (Blueprint $table) {
             $table->id();
             $table->string('email');
+            $table->ipAddress();
             $table->timestamps();
         });
     }
